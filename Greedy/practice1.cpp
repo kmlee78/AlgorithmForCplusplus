9a9dfd2ed448a8cebc6aae7ae700b006c6bdeb12
kmlee78 <jaoming787@gmail.com>
2020-11-19 23:40:55 +0900

    Update practice1.cpp

--- Greedy/practice1.cpp
+++ Greedy/practice1.cpp
@@ -12,22 +12,19 @@ int main(){
         cin >> fear;
         guild.push_back(fear);
     }
-    
-    /*가장 큰 값이 맨 오른쪽으로 갈 수 있도록 정렬*/
-    sort(guild.begin(), guild.end());
-    int lastNum = guild[N-1];
-    int index = N-1;
 
-    /*guild 벡터의 맨 마지막에서부터 각 자리에 해당하는
-      숫자 만큼 인덱스 이동. 이동 마다 answer 하나씩 증가*/  
+    /*guild 벡터의 오름차순 정렬*/
+    sort(guild.begin(), guild.end());
+    int lastNum = guild[0];
+    int index = 0;
+
+    /*guild 벡터의 첫 인덱스부터 각 자리에 해당하는
+      숫자 만큼 인덱스 이동. 이동 마다 answer 하나씩 증가*/
     while(1){
-        index -= lastNum;
-        if(index >= 0){
+        index += lastNum;
+        if(index < N){
             lastNum = guild[index];
             answer++;
-        }else if(index == -1){
-            answer++;
-            break;
         }else{
             break;
         }
